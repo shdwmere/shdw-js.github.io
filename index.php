0b83228cb2c4ecc2ab50649dbce25fcd1f91d6e9
delphiu-m <caioszrm1@gmail.com>
2023-02-09 21:10:36 -0300

    updated

--- index.php
+++ index.php
@@ -28,20 +28,22 @@
 
 <body>
     <!--Formulário de pagamento-->
-    <form action="process_payment.php" method="POST" id="form-checkout">
-        <div id="form-checkout__cardNumber" class="container"></div>
-        <div id="form-checkout__expirationDate" class="container"></div>
-        <div id="form-checkout__securityCode" class="container"></div>
-        <input type="text" id="form-checkout__cardholderName" />
-        <select id="form-checkout__issuer"></select>
-        <select id="form-checkout__installments"></select>
-        <select id="form-checkout__identificationType"></select>
-        <input type="text" id="form-checkout__identificationNumber" />
-        <input type="email" id="form-checkout__cardholderEmail" />
+    <div class="container">
+        <form action="process_payment.php" method="POST" id="form-checkout">
+            <div id="form-checkout__cardNumber" class="container"></div>
+            <div id="form-checkout__expirationDate" class="container"></div>
+            <div id="form-checkout__securityCode" class="container"></div>
+            <input type="text" id="form-checkout__cardholderName" />
+            <select id="form-checkout__issuer"></select>
+            <select id="form-checkout__installments"></select>
+            <select id="form-checkout__identificationType"></select>
+            <input type="text" id="form-checkout__identificationNumber" />
+            <input type="email" id="form-checkout__cardholderEmail" />
 
-        <button type="submit" id="form-checkout__submit">Pagar</button>
-        <progress value="0" class="progress-bar">Carregando...</progress>
-    </form>
+            <button type="submit" id="form-checkout__submit">Pagar</button>
+            <progress value="0" class="progress-bar">Carregando...</progress>
+        </form>
+    </div>
     <!--Formulário de pagamento-->
 
 
