216775918880050000748bf4c6143c5e39c35c72
delphiu-m <caioszrm1@gmail.com>
2023-02-09 21:06:47 -0300

    updated

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Front-end MercadoPago</title>
+    <style>
+        /*Estilização do formulário de pagamento*/
+        #form-checkout {
+            display: flex;
+            flex-direction: column;
+            max-width: 600px;
+        }
+
+        .container {
+            height: 18px;
+            display: inline-block;
+            border: 1px solid rgb(118, 118, 118);
+            border-radius: 2px;
+            padding: 1px 2px;
+        }
+
+        /*Estilização do formulário de pagamento*/
+    </style>
+</head>
+
+<body>
+    <!--Formulário de pagamento-->
+    <form action="process_payment.php" id="form-checkout">
+        <div id="form-checkout__cardNumber" class="container"></div>
+        <div id="form-checkout__expirationDate" class="container"></div>
+        <div id="form-checkout__securityCode" class="container"></div>
+        <input type="text" id="form-checkout__cardholderName" />
+        <select id="form-checkout__issuer"></select>
+        <select id="form-checkout__installments"></select>
+        <select id="form-checkout__identificationType"></select>
+        <input type="text" id="form-checkout__identificationNumber" />
+        <input type="email" id="form-checkout__cardholderEmail" />
+
+        <button type="submit" id="form-checkout__submit">Pagar</button>
+        <progress value="0" class="progress-bar">Carregando...</progress>
+    </form>
+    <!--Formulário de pagamento-->
+
+
+
+    <!--SDK Mercadopago.js-->
+    <script src="https://sdk.mercadopago.com/js/v2"></script>
+
+    <!--Scripts-->
+    <script src="index.js"></script>
+    <!--Scripts-->
+
+</body>
+
+</html>
---
+++ process_payment.php
@@ -0,0 +1,39 @@
+<?php
+// Encontre o estado do programa no campo status.
+
+require_once 'vendor/autoload.php';
+
+MercadoPago\SDK::setAccessToken("YOUR_ACCESS_TOKEN");
+
+// Instancia a classe Payment que provê os metodos para acessar a API que irá criar o pagamento. 
+$payment = new MercadoPago\Payment();
+$payment->transaction_amount = (float) $_POST['transactionAmount'];
+$payment->token = $_POST['token'];
+$payment->description = $_POST['description'];
+$payment->installments = (int) $_POST['installments'];
+$payment->payment_method_id = $_POST['paymentMethodId'];
+$payment->issuer_id = (int) $_POST['issuer'];
+
+// Criando novo pagador atribuindo os dados preenchidos no form
+$payer = new MercadoPago\Payer();
+$payer->email = $_POST['email'];
+$payer->identification = array(
+    "type" => $_POST['identificationType'],
+    "number" => $_POST['identificationNumber']
+);
+
+// pagador recebe a transação
+$payment->payer = $payer;
+
+// salva o pagamento
+$payment->save();
+
+//resposta do pagamento
+$response = array(
+    'status' => $payment->status,
+    'status_detail' => $payment->status_detail,
+    'id' => $payment->id
+);
+echo json_encode($response);
+
+?>
